a1631c9904ad496fc98227ae16887c3693814258
ASoldo <soldo.andrej@gmail.com>
2024-05-21 00:42:06 +0200

    Animate shader using tween instead of using update()

--- src/scenes/game_scene4.ts
+++ src/scenes/game_scene4.ts
@@ -5,15 +5,15 @@ import vertex from "../system/shaders/demo/vert_shader.glsl";
 import frag_noise from "../system/shaders/noise_shader/frag_noise.glsl";
 import { scene_manager } from "../core/core";
 import { SceneNames } from "../system/types/scene_names";
+import * as TWEEN from "@tweenjs/tween.js";
 
 export class GameScene4 extends BaseScene {
   private quad!: PIXI.Mesh;
   private noise_texture!: PIXI.Texture;
-  private elapsedTime: number;
+  private tween!: TWEEN.Tween<Number> | null;
 
   constructor(app: PIXI.Application, engine: Engine, render: Render) {
     super(app, engine, render);
-    this.elapsedTime = 0;
   }
 
   async preload(): Promise<void> {
@@ -55,7 +55,7 @@ export class GameScene4 extends BaseScene {
 
       resources: {
         noise_uniforms: {
-          limit: { type: "f32", value: 0.5 },
+          limit: { type: "f32", value: 0.0 },
           color: { type: "vec3<f32>", value: [0.0, 0.0, 0.0] },
         },
         noise: this.noise_texture.source,
@@ -82,32 +82,36 @@ export class GameScene4 extends BaseScene {
     // Set the loaded flag
     this.setLoaded(true);
 
+    this.tween = new TWEEN.Tween(
+      this.quad.shader.resources.noise_uniforms.uniforms,
+    )
+      .to({ limit: 1 }, 1500)
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .yoyo(true)
+      .repeat(Infinity)
+      .start();
+
     setTimeout(() => {
       scene_manager.goToScene(SceneNames.SCENE5);
     }, 3000);
   }
 
-  async update(deltaTime: number): Promise<void> {
+  async update(_deltaTime: number): Promise<void> {
     if (!this.active) return; // Only update if the scene is active
 
-    this.elapsedTime += deltaTime;
-
-    // Update the rotation of the meshes
-    // this.quad.rotation += deltaTime * 0.01;
-
-    this.quad.shader.resources.noise_uniforms.uniforms.limit = Math.abs(
-      Math.sin(this.elapsedTime * 0.005),
-    );
+    TWEEN.update();
   }
 
   async destroy(): Promise<void> {
     console.log("Destroying Game: ", this.name);
 
     // Reset scene variables
-    this.elapsedTime = 0;
 
     // Remove meshes from the stage and destroy them
     this.app.stage.removeChild(this.quad);
     this.quad.destroy();
+
+    this.tween?.stop();
+    this.tween = null;
   }
 }
