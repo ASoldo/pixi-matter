51997f8de240b9a65a8fed58facce9b88d3c7b45
ASoldo <soldo.andrej@gmail.com>
2024-05-23 01:29:32 +0200

    Add CapacitorJs for Android/iOs support
    Notice: Video as Sprite texture is laggy on .apk and .ipa. Don't use it!

--- src/scenes/game_scene5.ts
+++ src/scenes/game_scene5.ts
@@ -5,8 +5,8 @@ import { scene_manager } from "../core/core";
 import { SceneNames } from "../system/types/scene_names";
 
 export class GameScene5 extends BaseScene {
-  public texture!: PIXI.Texture;
-  public videoSprite!: PIXI.Sprite;
+  // public texture!: PIXI.Texture;
+  // public videoSprite!: PIXI.Sprite;
 
   constructor(app: PIXI.Application, engine: Engine, render: Render) {
     super(app, engine, render);
@@ -21,31 +21,80 @@ export class GameScene5 extends BaseScene {
     this.loaded = false;
 
     // Load the video texture
-    this.texture = await PIXI.Assets.load({
-      src: "https://pixijs.com/assets/video.mp4",
-      data: {
-        muted: true,
-      },
-      type: "video",
-    });
-
-    // Create a PIXI sprite using the video texture
-    this.videoSprite = new PIXI.Sprite(this.texture);
-    this.videoSprite.position.set(0, 0);
-    this.videoSprite.width = this.app.screen.width;
-    this.videoSprite.height = this.app.screen.height;
-    this.app.stage.addChild(this.videoSprite);
+    // this.texture = await PIXI.Assets.load({
+    //   src: "https://videos.pexels.com/video-files/14597401/14597401-hd_1920_1080_24fps.mp4",
+    //   data: {
+    //     muted: true,
+    //   },
+    //   type: "video",
+    // });
+    //
+    // // Create a PIXI sprite using the video texture
+    // this.videoSprite = new PIXI.Sprite(this.texture);
+    // this.videoSprite.position.set(0, 0);
+    // this.videoSprite.width = this.app.screen.width;
+    // this.videoSprite.height = this.app.screen.height;
+    // this.app.stage.addChild(this.videoSprite);
 
     // Example of how to pause the video
     // (this.texture.source.resource as HTMLVideoElement).pause();
 
-    // Set a timeout to switch to the next scene after the video ends
+    // Add font files to the bundle
+    PIXI.Assets.addBundle("fonts", [
+      {
+        alias: "ChaChicle",
+        src: "https://pixijs.com/assets/webfont-loader/ChaChicle.ttf",
+      },
+      {
+        alias: "Lineal",
+        src: "https://pixijs.com/assets/webfont-loader/Lineal.otf",
+      },
+      {
+        alias: "Dotrice Regular",
+        src: "https://pixijs.com/assets/webfont-loader/Dotrice-Regular.woff",
+      },
+      {
+        alias: "Crosterian",
+        src: "https://pixijs.com/assets/webfont-loader/Crosterian.woff2",
+      },
+    ]);
+
+    // Load the font bundle
+    await PIXI.Assets.loadBundle("fonts");
+
+    const text1 = new PIXI.Text({
+      text: "ChaChicle.ttf",
+      style: { fontFamily: "ChaChicle", fontSize: 50 },
+    });
+    const text2 = new PIXI.Text({
+      text: "Lineal.otf",
+      style: { fontFamily: "Lineal", fontSize: 50 },
+    });
+    const text3 = new PIXI.Text({
+      text: "Dotrice Regular.woff",
+      style: { fontFamily: "Dotrice Regular", fontSize: 50 },
+    });
+    const text4 = new PIXI.Text({
+      text: "Crosterian.woff2",
+      style: { fontFamily: "Crosterian", fontSize: 50 },
+    });
+
+    text2.y = 150;
+    text3.y = 300;
+    text4.y = 452;
+
+    this.app.stage.addChild(text1);
+    this.app.stage.addChild(text2);
+    this.app.stage.addChild(text3);
+    this.app.stage.addChild(text4);
+
     setTimeout(
       () => {
         scene_manager.goToScene(SceneNames.SCENE1);
       },
-      (this.texture.source.resource as HTMLVideoElement).duration * 1000,
-    ); // Convert seconds to milliseconds
+      /* (this.texture.source.resource as HTMLVideoElement).duration * 1000, */
+      3000,
+    );
 
     this.setLoaded(true);
   }
@@ -55,8 +104,12 @@ export class GameScene5 extends BaseScene {
   }
 
   async destroy(): Promise<void> {
-    await PIXI.Assets.unload(this.texture.source.label);
-    this.videoSprite.destroy();
+    // await PIXI.Assets.unload(this.texture.source.label);
+    // this.videoSprite.destroy();
     console.log("Destroying Game: ", this.name);
+    await PIXI.Assets.unloadBundle("ChaChicle");
+    await PIXI.Assets.unloadBundle("Lineal");
+    await PIXI.Assets.unloadBundle("Dotrice Regular");
+    await PIXI.Assets.unloadBundle("Crosterian");
   }
 }
