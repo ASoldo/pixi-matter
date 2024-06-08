a3ad8877458eaab86e188486a6c64e838ccb3b71
ASoldo <soldo.andrej@gmail.com>
2024-06-08 20:22:15 +0200

    Update Create migration and delete updated migration

--- pb_migrations/1717865242_created_asset.js
+++ pb_migrations/1717865242_created_asset.js
@@ -1,73 +1,73 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const collection = new Collection({
-    "id": "dmmayaglednbdnw",
-    "created": "2024-06-08 16:47:22.859Z",
-    "updated": "2024-06-08 16:47:22.859Z",
-    "name": "asset",
-    "type": "base",
-    "system": false,
-    "schema": [
-      {
-        "system": false,
-        "id": "xfietc83",
-        "name": "alias",
-        "type": "text",
-        "required": false,
-        "presentable": false,
-        "unique": false,
-        "options": {
-          "min": null,
-          "max": null,
-          "pattern": ""
-        }
-      },
-      {
-        "system": false,
-        "id": "sr5yppw2",
-        "name": "src",
-        "type": "url",
-        "required": false,
-        "presentable": false,
-        "unique": false,
-        "options": {
-          "exceptDomains": null,
-          "onlyDomains": null
-        }
-      },
-      {
-        "system": false,
-        "id": "jfp4e9yt",
-        "name": "img",
-        "type": "file",
-        "required": false,
-        "presentable": false,
-        "unique": false,
-        "options": {
-          "mimeTypes": [
-            "image/png",
-            "image/jpeg"
-          ],
-          "thumbs": [],
-          "maxSelect": 1,
-          "maxSize": 5242880,
-          "protected": false
-        }
-      }
-    ],
-    "indexes": [],
-    "listRule": null,
-    "viewRule": null,
-    "createRule": null,
-    "updateRule": null,
-    "deleteRule": null,
-    "options": {}
-  });
+migrate(
+  (db) => {
+    const collection = new Collection({
+      id: "dmmayaglednbdnw",
+      created: "2024-06-08 16:47:22.859Z",
+      updated: "2024-06-08 16:47:22.859Z",
+      name: "assets",
+      type: "base",
+      system: false,
+      schema: [
+        {
+          system: false,
+          id: "xfietc83",
+          name: "alias",
+          type: "text",
+          required: false,
+          presentable: false,
+          unique: false,
+          options: {
+            min: null,
+            max: null,
+            pattern: "",
+          },
+        },
+        {
+          system: false,
+          id: "sr5yppw2",
+          name: "src",
+          type: "url",
+          required: false,
+          presentable: false,
+          unique: false,
+          options: {
+            exceptDomains: null,
+            onlyDomains: null,
+          },
+        },
+        {
+          system: false,
+          id: "jfp4e9yt",
+          name: "img",
+          type: "file",
+          required: false,
+          presentable: false,
+          unique: false,
+          options: {
+            mimeTypes: ["image/png", "image/jpeg"],
+            thumbs: [],
+            maxSelect: 1,
+            maxSize: 5242880,
+            protected: false,
+          },
+        },
+      ],
+      indexes: [],
+      listRule: null,
+      viewRule: null,
+      createRule: null,
+      updateRule: null,
+      deleteRule: null,
+      options: {},
+    });
 
-  return Dao(db).saveCollection(collection);
-}, (db) => {
-  const dao = new Dao(db);
-  const collection = dao.findCollectionByNameOrId("dmmayaglednbdnw");
+    return Dao(db).saveCollection(collection);
+  },
+  (db) => {
+    const dao = new Dao(db);
+    const collection = dao.findCollectionByNameOrId("dmmayaglednbdnw");
 
-  return dao.deleteCollection(collection);
-})
+    return dao.deleteCollection(collection);
+  },
+);
